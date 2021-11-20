06e8d3aee3d92243875f4779e5b5d36a4c1cf033
Chen <19492223+chenqi0805@users.noreply.github.com>
2021-11-19 19:29:15 -0600

    ADD: initial sampler and examples
    Signed-off-by: Chen <19492223+chenqi0805@users.noreply.github.com>

---
+++ src/main/java/com/amazon/dataprepper/log/generator/ApacheLogGenerator.java
@@ -0,0 +1,60 @@
+package com.amazon.dataprepper.log.generator;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Internet;
+import com.linecorp.armeria.common.HttpData;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class ApacheLogGenerator {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final String[] HTTP_METHODS = new String[] {"GET", "POST", "DELETE", "PUT"};
+    private final String[] FAKE_URIS = new String[] {"/list", "/explore", "/search/tag/list", "/apps/cart.jsp?appID="};
+    private final String[] FAKE_STATUS = new String[] {"200", "404", "500", "301"};
+    private final String APACHE_LOG_FORMAT = "%s %s %s [%s] \"%s %s HTTP/1.0\" %s %s \"http://%s\" \"%s\"";
+    private final Faker faker = new Faker();
+    private final Random random = new Random();
+    private final DateFormat dateFormat = new SimpleDateFormat("dd/MMM/y:HH:mm:ss Z");
+
+    public String generateRandomApacheLog() {
+        final Internet internet = faker.internet();
+        final String username = faker.name().username();
+        final String password = internet.password();
+        final String httpMethod = HTTP_METHODS[random.nextInt(4)];
+        final String uri = FAKE_URIS[random.nextInt(4)];
+        final String status = FAKE_STATUS[random.nextInt(4)];
+        final int bytes = random.nextInt(1001) + 4000;
+        return String.format(
+                APACHE_LOG_FORMAT,
+                internet.ipV4Address(),
+                username,
+                password,
+                dateFormat.format(faker.date().past(100, TimeUnit.DAYS)),
+                httpMethod,
+                uri,
+                status,
+                bytes,
+                internet.url(),
+                internet.userAgentAny()
+        );
+    }
+
+    public HttpData generateRandomApacheLogHttpData(final int numLogs) throws JsonProcessingException {
+        final List<Map<String, Object>> jsonArray = new ArrayList<>();
+        for (int i = 0; i < numLogs; i++) {
+            final Map<String, Object> logObj = new HashMap<String, Object>() {{
+                put("date", System.currentTimeMillis());
+                put("log", generateRandomApacheLog());
+            }};
+            jsonArray.add(logObj);
+        }
+        final String jsonData = objectMapper.writeValueAsString(jsonArray);
+        return HttpData.ofUtf8(jsonData);
+    }
+}
---
+++ src/main/java/com/amazon/dataprepper/log/generator/CleanSampler.java
@@ -0,0 +1,98 @@
+package com.amazon.dataprepper.log.generator;
+
+import com.linecorp.armeria.client.WebClient;
+import com.linecorp.armeria.common.*;
+import io.micrometer.core.instrument.util.StringUtils;
+import org.apache.jmeter.config.Arguments;
+import org.apache.jmeter.protocol.java.sampler.AbstractJavaSamplerClient;
+import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
+import org.apache.jmeter.samplers.SampleResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+
+public class CleanSampler extends AbstractJavaSamplerClient implements Serializable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CleanSampler.class);
+
+    private static final String LOG_URI_PATH = "/log/ingest";
+
+    private static final String TARGET_ADDRESS = "targetAddress";
+    private static final String URI_PATH = "uri";
+    private static final String TIMEOUT_MS = "timeoutMs";
+    private static final String TARGET_PORT = "targetPort";
+    private static final String NUM_LOGS = "numLogs";
+    private final ApacheLogGenerator apacheLogGenerator = new ApacheLogGenerator();
+
+    int timeoutMilliseconds = 10000;
+
+    WebClient webClient;
+    String targetWithPort;
+    String uri;
+    int numLogs = 20;
+
+    public void setupTest(JavaSamplerContext javaSamplerContext) {
+        String targetAddress = javaSamplerContext.getParameter(TARGET_ADDRESS);
+        String targetPort = javaSamplerContext.getParameter(TARGET_PORT);
+        uri = javaSamplerContext.getParameter(URI_PATH, "/");
+        targetWithPort = targetAddress + ":" + targetPort;
+
+        String timeoutMs = javaSamplerContext.getParameter(TIMEOUT_MS);
+        this.timeoutMilliseconds = Integer.parseInt(timeoutMs);
+
+
+        webClient = WebClient.builder("http://" + targetWithPort)
+                .responseTimeoutMillis(timeoutMilliseconds)
+                .build();
+
+        String numLogsArg = javaSamplerContext.getParameter(NUM_LOGS);
+        if (!StringUtils.isEmpty(numLogsArg)) {
+            numLogs = Integer.valueOf(numLogsArg);
+        }
+    }
+
+    @Override
+    public Arguments getDefaultParameters() {
+
+        Arguments defaultParameters = new Arguments();
+        defaultParameters.addArgument(TARGET_ADDRESS, "localhost");
+        defaultParameters.addArgument(URI_PATH, LOG_URI_PATH);
+        defaultParameters.addArgument(TIMEOUT_MS, "10000");
+        defaultParameters.addArgument(TARGET_PORT, "2021");
+        defaultParameters.addArgument(NUM_LOGS, "20");
+
+        return defaultParameters;
+    }
+
+    @Override
+    public SampleResult runTest(JavaSamplerContext javaSamplerContext) {
+
+        SampleResult sampleResult = new SampleResult();
+        sampleResult.sampleStart();
+
+        try {
+            final HttpData httpData = apacheLogGenerator.generateRandomApacheLogHttpData(numLogs);
+
+            HttpResponse response = webClient.execute(RequestHeaders.builder()
+                    .scheme(SessionProtocol.HTTP)
+                    .authority(targetWithPort)
+                    .method(HttpMethod.POST)
+                    .path(uri)
+                    .contentType(MediaType.JSON_UTF_8)
+                    .build(), httpData);
+
+            response.aggregate().get();
+
+            sampleResult.sampleEnd();
+            sampleResult.setSuccessful(Boolean.TRUE);
+            sampleResult.setResponseCodeOK();
+        } catch (Exception e) {
+            LOGGER.error("Request was not successfully processed", e);
+            sampleResult.sampleEnd();
+            sampleResult.setResponseMessage(e.getMessage());
+            sampleResult.setSuccessful(Boolean.FALSE);
+        }
+
+        return sampleResult;
+    }
+}
